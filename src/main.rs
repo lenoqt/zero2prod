29a321595df04f64c3491ac7364258ac25071f87
Gustavo Barrios <gustavoantonio51@gmail.com>
2022-08-15 00:47:33 +0900

    continue 3.4

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpServer, HttpResponse};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/heatlh_check", web::get().to(health_check))
+            .route("/health_check", web::get().to(health_check))
     })
         .bind("127.0.0.1:8000")?
         .run()
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::http::StatusCode;
+    use crate::health_check;
+
+    #[actix_web::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        // This requires changing the return type of `health_check`
+        // from `impl Responder` to `HttpResponse` to compile
+        // You also need to import it with the `use actix_web::HttpResponse`
+        assert_eq!(response.status(), StatusCode::OK)
+    }
 }
