6c81413db3135aa6443d4c156c50f977c364dd57
Gustavo Barrios <gustavoantonio51@gmail.com>
2022-08-16 00:07:26 +0900

    finished 3.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run();
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let address = listener.local_addr().unwrap().to_string();
+    log::info!("Starting HTTP at {address}");
+
+    let server = HttpServer::new(|| {
+        App::new().route(
+            "/health_check",
+            web::get().to(health_check).wrap(Logger::default()),
+        )
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
