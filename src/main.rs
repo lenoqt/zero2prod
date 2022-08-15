55b178a8ab3764ccb4691987ee2ef531412110d7
Gustavo Barrios <gustavoantonio51@gmail.com>
2022-08-15 14:16:22 +0900

    finished 3.4.2

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::health_check;
-    use actix_web::http::StatusCode;
-
-    #[actix_web::test]
-    async fn health_check_succeeds() {
-        let response = health_check().await;
-        // This requires changing the return type of `health_check`
-        // from `impl Responder` to `HttpResponse` to compile
-        // You also need to import it with the `use actix_web::HttpResponse`
-        assert_eq!(response.status(), StatusCode::OK)
-    }
+    run()?.await
 }
