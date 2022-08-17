cadb0468e2446737c14c39cc5f945ee97b0db40c
Gustavo Barrios <gustavoantonio51@gmail.com>
2022-08-18 03:09:41 +0900

    finished 3.7, starting 3.8

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,42 @@
 use actix_web::dev::Server;
 use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
+use std::sync::Once;
+
+static INIT: Once = Once::new();
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn setup_logger() {
+    INIT.call_once(|| {
+        let env = env_logger::Env::new().default_filter_or("info");
+        env_logger::init_from_env(env);
+    });
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let address = listener.local_addr().unwrap().to_string();
     log::info!("Starting HTTP at {address}");
 
     let server = HttpServer::new(|| {
-        App::new().route(
-            "/health_check",
-            web::get().to(health_check).wrap(Logger::default()),
-        )
+        App::new()
+            .route(
+                "/health_check",
+                web::get().to(health_check).wrap(Logger::default()),
+            )
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::{run, setup_logger};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    setup_logger();
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     run(listener)?.await
 }
