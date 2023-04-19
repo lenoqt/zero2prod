cebdb41756c88432af295da92233f3ad66a8f4d2
Gustavo Barrios <gustavoantonio51@gmail.com>
2023-04-19 09:35:50 +0000

    test for vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-yum install clang llvm -y
+yum install clang llvm binutils -y
 curl --proto 'https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+echo '[target.x86_64-unknown-linux-gnu]
+rustflags = ["-C", "linker=clang", "-C", "link-arg=-fuse-ld=ld"]' >> ~/.cargo/config.toml
 source "$HOME/.cargo/env"
-cargo build --release
+cargo install cargo-chef
+cargo chef prepare --recipe-path recipe.json
+cargo chef cook --release --recipe-path recipe.json
+SQLX_OFFLINE=true cargo build --release --bin zero2prod
