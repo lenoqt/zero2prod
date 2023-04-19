01695ef9377ac9121b7bbe640a6455cec9732b2d
Gustavo Barrios <gustavoantonio51@gmail.com>
2023-04-19 04:53:56 +0000

    test for vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
+yum install clang lld -y
 curl --proto 'https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
-apt update && apt install lld clang -y
 cargo build --release
