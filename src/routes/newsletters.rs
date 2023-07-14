3173ae3c0fd7125240d244114b0bcfd5c381c4a6
Gustavo Barrios <gustavoantonio51@gmail.com>
2023-07-14 23:29:47 +0900

    Pushing missing files.

---
+++ src/routes/newsletters.rs
@@ -0,0 +1,125 @@
+//! src/routes/newsletters.rs
+
+use crate::domain::SubscriberEmail;
+use crate::email_client::EmailClient;
+use crate::routes::error_chain_fmt;
+use actix_web::http::StatusCode;
+use actix_web::{web, HttpResponse, ResponseError};
+use anyhow::Context;
+use sqlx::PgPool;
+use std::fmt::Formatter;
+
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+pub async fn publish_newsletter(
+    body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+) -> Result<HttpResponse, PublishError> {
+    let subscribers = get_confirmed_subscribers(&pool).await?;
+    for subscriber in subscribers {
+        // The compiler forces us to handle both cases `Ok` & `Err`
+        match subscriber {
+            Ok(subscriber) => {
+                email_client
+                    .send_email(
+                        &subscriber.email,
+                        &body.title,
+                        &body.content.html,
+                        &body.content.text,
+                    )
+                    .await
+                    // Use `with_context` when there's a runtime cost. format macro stores the string into
+                    // the heap, using `context` would be allocating that string every time we send an
+                    // email out.
+                    .with_context(|| {
+                        format!("Failed to send newsletter issue to {}", subscriber.email)
+                    })?;
+            }
+            Err(error) => {
+                tracing::warn!(
+                    // We record the error chain as a structured field
+                    // on the log record.
+                    error.cause_chain = ?error,
+                    // Using `\` to split a long string literal over
+                    // two lines, without creating a `\n` character.
+                    "Skipping a confirmed subscriber. \
+                    Their stored contact details are invalid"
+                )
+            }
+        }
+    }
+    Ok(HttpResponse::Ok().finish())
+}
+
+struct ConfirmedSubscriber {
+    email: SubscriberEmail,
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+    // We are returning a `Vec` of `Result`s in the happy case
+    // This allows the caller to bubble up errors due to network issues or other
+    // transient failures using the `?` operator, while the compiler
+    // forces them to handle the subtler mapping error.
+    // See http://sled.rs/errors.html for a deep-dive about this technique.
+) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
+    // We only need `Row` to map the data coming out of this query.
+    // Nesting its definition inside the function itself is a simple way
+    // to clearly communicate this coupling (and to ensure it doesn't
+    // get used elsewhere by mistake).
+    struct Row {
+        email: String,
+    }
+
+    let rows = sqlx::query_as!(
+        Row,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+    // Map into domain type
+    let confirmed_subscribers = rows
+        .into_iter()
+        .map(|r| match SubscriberEmail::parse(r.email) {
+            Ok(email) => Ok(ConfirmedSubscriber { email }),
+            Err(error) => Err(anyhow::anyhow!(error)),
+        })
+        .collect();
+    Ok(confirmed_subscribers)
+}
